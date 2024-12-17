9fd624cfd646d91d27bf8383e051419ceb77dc95
Angel <91931104+AngelPortilla1@users.noreply.github.com>
2024-12-17 01:20:21 -0500

    cambio de url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    api: 'https://cursowebbackend.onrender.com/api',  // URL de producción
+    api: 'https://cursowebbackend.onrender.com/api/emails',  // URL de producción
   };
   
